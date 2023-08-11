b5f728a98a8737ea487a7bbee5e07f09aa6dd861
Anton Pashkovskyi <125904314+antonpashdev@users.noreply.github.com>
2023-08-11 14:01:45 +0200

    Create payment.ts

---
+++ pages/api/payment.ts
@@ -0,0 +1,12 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from 'next';
+import fs from 'fs';
+
+export default function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Data>
+) {
+  const { paymentId, ids = [] } = req.body;
+  fs.writeFileSync(`downloads/${paymentId}`, `${ids.join(';')}`, 'utf8');
+  res.status(200);
+}
