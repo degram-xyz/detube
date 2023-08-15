f113a7a14d0b5816b12eefcdb3edb0f7dd72b5cd
Anton Pashkovskyi <125904314+antonpashdev@users.noreply.github.com>
2023-08-15 13:46:02 +0200

    build

--- pages/api/payment.ts
+++ pages/api/payment.ts
@@ -6,27 +6,27 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  const { paymentId, ids = [] } = req.body;
-  const key = `payment_${paymentId}`;
-  const payment = JSON.parse(await get(key));
-  payment.isPayed = true;
-  const body = JSON.stringify({
-    "items": [
-      {
-        "operation": "update",
-        "key": key,
-        "value": JSON.stringify(payment),
-      }
-    ]
-  });
-  const response = await fetch('https://api.vercel.com/v1/edge-config/ecfg_j2bcrab70dm7klciie740bsuwm5x/items?teamId=team_w9RYKh8OIpPW35JwxcUWSK8X', {
-    method: 'PATCH',
-    headers: {
-      'Authorization': `Bearer ${process.env.VERCEL_TOKEN}`,
-      'Content-Type': 'application/json'
-    },
-    body,
-  });
-  console.log(`${response.status}: ${JSON.stringify(await response.json())}`);
-  res.status(response.status).json(await response.json());
+  // const { paymentId, ids = [] } = req.body;
+  // const key = `payment_${paymentId}`;
+  // const payment = JSON.parse(await get(key));
+  // payment.isPayed = true;
+  // const body = JSON.stringify({
+  //   "items": [
+  //     {
+  //       "operation": "update",
+  //       "key": key,
+  //       "value": JSON.stringify(payment),
+  //     }
+  //   ]
+  // });
+  // const response = await fetch('https://api.vercel.com/v1/edge-config/ecfg_j2bcrab70dm7klciie740bsuwm5x/items?teamId=team_w9RYKh8OIpPW35JwxcUWSK8X', {
+  //   method: 'PATCH',
+  //   headers: {
+  //     'Authorization': `Bearer ${process.env.VERCEL_TOKEN}`,
+  //     'Content-Type': 'application/json'
+  //   },
+  //   body,
+  // });
+  // console.log(`${response.status}: ${JSON.stringify(await response.json())}`);
+  // res.status(response.status).json(await response.json());
 }
