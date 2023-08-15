e0f49606628b7b9d544e8152f78bf4e647914305
Anton Pashkovskyi <125904314+antonpashdev@users.noreply.github.com>
2023-08-15 12:43:07 +0200

    build

--- pages/api/payment.ts
+++ pages/api/payment.ts
@@ -7,17 +7,18 @@ export default async function handler(
   res: NextApiResponse
 ) {
   const { paymentId, ids = [] } = req.body;
+  const body = JSON.stringify({
+    "items": [
+      {
+        "operation": "update",
+        "key": "foo",
+        "value": "bar"
+      }
+    ]
+  });
   await fetch('https://api.vercel.com/v1/edge-config/ecfg_j2bcrab70dm7klciie740bsuwm5x/items', {
     method: 'POST',
-    body: {
-      "items": [
-        {
-          "operation": "update",
-          "key": "foo",
-          "value": "bar"
-        }
-      ]
-    },
+    body,
   });
   res.status(200);
 }
