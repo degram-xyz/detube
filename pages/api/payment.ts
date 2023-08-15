a9fe9ef6975ff03cb771febb50041c42de19748f
Anton Pashkovskyi <125904314+antonpashdev@users.noreply.github.com>
2023-08-15 12:39:02 +0200

    build

--- pages/api/payment.ts
+++ pages/api/payment.ts
@@ -1,6 +1,6 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next';
-import edge from '@vercel/edge-config';
+import * as edge from '@vercel/edge-config';
 
 export default async function handler(
   req: NextApiRequest,
