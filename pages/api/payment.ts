d6fc54ffbd79752b2d106d4ecb4a1718a863ee93
Anton Pashkovskyi <125904314+antonpashdev@users.noreply.github.com>
2023-08-15 12:41:38 +0200

    build

--- pages/api/payment.ts
+++ pages/api/payment.ts
@@ -1,15 +1,23 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next';
-import * as edge from '@vercel/edge-config';
+import { get } from '@vercel/edge-config';
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
   const { paymentId, ids = [] } = req.body;
-  // await fetch('https://api.vercel.com/v1/edge-config/ecfg_j2bcrab70dm7klciie740bsuwm5x/items', {
-  //   method: 'POST'
-  // });
-  console.log(Object.getOwnPropertyNames(edge));
+  await fetch('https://api.vercel.com/v1/edge-config/ecfg_j2bcrab70dm7klciie740bsuwm5x/items', {
+    method: 'POST',
+    body: {
+      "items": [
+        {
+          "operation": "update",
+          "key": "foo",
+          "value": "bar"
+        }
+      ]
+    },
+  });
   res.status(200);
 }
