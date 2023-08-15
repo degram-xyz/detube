a39a40b50383ddfda0c6d772224c5c2785ff692c
Anton Pashkovskyi <125904314+antonpashdev@users.noreply.github.com>
2023-08-15 13:28:35 +0200

    build

--- pages/api/payment.ts
+++ pages/api/payment.ts
@@ -24,7 +24,7 @@ export default async function handler(
     },
     body,
   });
-  console.log(`${response.status}: ${await response.json()}`);
+  console.log(`${response.status}: ${JSON.stringify(await response.json())}`);
   const items = await getAll();
   res.status(200).json(items);
 }
