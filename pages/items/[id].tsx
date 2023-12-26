827e8afd349b51ad0a5e6c76e77335ccdba875f9
Anton Pashkovskyi <125904314+antonpashdev@users.noreply.github.com>
2023-12-26 20:33:11 +0100

    sdfsdf

--- pages/items/[id].tsx
+++ pages/items/[id].tsx
@@ -35,13 +35,9 @@ const Item: NextPage<{ products: any[] }> = ({ products }) => {
         <div className="bg-white">
           <div className="mx-auto max-w-2xl py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
             <div className="lg:grid lg:grid-cols-2 lg:items-start lg:gap-x-8">
-              <Tab.Group as="div" className="flex flex-col-reverse">
-                <Tab.Panels className="aspect-w-1 aspect-h-1 w-full">
-                  <Tab.Panel>
-                    <object data={product.link}></object>
-                  </Tab.Panel>
-                </Tab.Panels>
-              </Tab.Group>
+              <div>
+                <object className="aspect-video" data={product.link}></object>
+              </div>
 
               {/* Product info */}
               <div className="mt-10 px-4 sm:mt-16 sm:px-0 lg:mt-0">
